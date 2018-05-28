c31bb13d04ab19fe52a781de79fbe6708f424700
Masaharu Takahashi <m-taka@availjp.com>
2018-05-28 16:15:30 +0900

    ToiletCheckerWinアプリプロジェクトを追加した。 少しだけメイクした。

---
+++ ToiletChecker/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ToiletChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_big = new System.Windows.Forms.Button();
+            this.button_small = new System.Windows.Forms.Button();
+            this.button_big_small = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_big
+            // 
+            this.button_big.Location = new System.Drawing.Point(30, 65);
+            this.button_big.Name = "button_big";
+            this.button_big.Size = new System.Drawing.Size(75, 75);
+            this.button_big.TabIndex = 0;
+            this.button_big.Text = "大";
+            this.button_big.UseVisualStyleBackColor = true;
+            this.button_big.Click += new System.EventHandler(this.button_big_Click);
+            // 
+            // button_small
+            // 
+            this.button_small.Location = new System.Drawing.Point(152, 65);
+            this.button_small.Name = "button_small";
+            this.button_small.Size = new System.Drawing.Size(75, 75);
+            this.button_small.TabIndex = 1;
+            this.button_small.Text = "小";
+            this.button_small.UseVisualStyleBackColor = true;
+            this.button_small.Click += new System.EventHandler(this.button_small_Click);
+            // 
+            // button_big_small
+            // 
+            this.button_big_small.Location = new System.Drawing.Point(277, 65);
+            this.button_big_small.Name = "button_big_small";
+            this.button_big_small.Size = new System.Drawing.Size(75, 75);
+            this.button_big_small.TabIndex = 2;
+            this.button_big_small.Text = "大小";
+            this.button_big_small.UseVisualStyleBackColor = true;
+            this.button_big_small.Click += new System.EventHandler(this.button_big_small_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(63, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ボタンを押してください。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 287);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_big);
+            this.Controls.Add(this.button_small);
+            this.Controls.Add(this.button_big_small);
+            this.Name = "Form1";
+            this.Text = "Toilet Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_big;
+        private System.Windows.Forms.Button button_small;
+        private System.Windows.Forms.Button button_big_small;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ToiletChecker/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToiletChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_small_Click(object sender, EventArgs e)
+        {
+            DateTime dtTime = DateTime.Now;
+            label1.Text = dtTime.ToString() + "に小をしました。";
+            WriteTextToiletTime( dtTime, "小" );
+
+
+        }
+
+        private void button_big_Click(object sender, EventArgs e)
+        {
+            DateTime dtTime = DateTime.Now;
+            label1.Text = dtTime.ToString() + "に大をしました。";
+            WriteTextToiletTime( dtTime, "大" );
+        }
+
+        private void button_big_small_Click(object sender, EventArgs e)
+        {
+            DateTime dtTime = DateTime.Now;
+            label1.Text = dtTime.ToString() + "に大小をしました。";
+            WriteTextToiletTime( dtTime , "大小" );
+        }
+
+        private void WriteTextToiletTime( DateTime dtTime , string str )
+        {
+            Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+            StreamWriter writer =
+              new StreamWriter(@"ToiletChecker.txt", true, sjisEnc);
+            writer.WriteLine( dtTime.ToShortDateString() + " " + dtTime.ToShortTimeString() + "," + str );
+            writer.Close();
+        }
+    }
+}
